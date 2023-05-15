736e48052344bd3f019ec59cb2af9c603138987e
Pedro Fernandes <thephfernandes@gmail.com>
2023-05-15 18:06:14 -0300

    extends date utils

--- app/utils/date.ts
+++ app/utils/date.ts
@@ -7,7 +7,28 @@ export const formatDate = (dateString: string | Date) => {
 	return `${year}-${month}-${day}`;
 };
 
-export const parseDateString = (dateString: string) => {
-	const [year, month, day] = dateString.split('-').map(Number);
-	return new Date(year, month - 1, day);
+export const parseDateString = (dateString: string, eod = false) => {
+	const [year, month, day] = dateString.split("-").map(Number);
+	const date = new Date(year, month - 1, day);
+
+	if (eod) {
+		date.setHours(23, 59, 59, 999);
+	}
+
+	return date;
+}
+
+export const formatDateLabel = (date: Date) => {
+	const year = String(date.getFullYear());
+	let month = String(date.getMonth() + 1);
+	let day = String(date.getDate());
+	if (month.length < 2) {
+		month = String(month).padStart(2, "0");
+	}
+
+	if (day.length < 2) {
+		day = String(day).padStart(2, "0");
+	}
+
+	return [day, month, year].join("/");
 };
