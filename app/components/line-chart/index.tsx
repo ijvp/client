58dad8e36a8a1c5412a97cfc57e66ef6c8dcb203
Pedro Fernandes <thephfernandes@gmail.com>
2023-05-17 17:11:58 -0300

    adds prop types and more styling

--- app/components/line-chart/index.tsx
+++ app/components/line-chart/index.tsx
@@ -1,5 +1,6 @@
 import type { LinksFunction } from "@remix-run/node";
 import { useEffect, useState } from "react";
+import type { DataPoint } from '@shopify/polaris-viz';
 import { SparkLineChart } from '@shopify/polaris-viz';
 import styles from "./styles.css";
 
@@ -7,7 +8,13 @@ export const links: LinksFunction = () => [
 	{ rel: "stylesheet", href: styles }
 ];
 
-export default function LineChart({ data, title, value }) {
+interface LineChartProps {
+	data: DataPoint[],
+	title: string,
+	value: number | string,
+	prefix?: string
+}
+export default function LineChart({ data, title, value, prefix }: LineChartProps) {
 	const [render, setRender] = useState(false);
 
 	useEffect(() => {
@@ -20,16 +27,30 @@ export default function LineChart({ data, title, value }) {
 		<div
 			className="
 			nodge
-			max-w-[345px] h-[230px] w-full 
-			bg-black-secondary p-4 pb-24 rounded-lg
+			w-[356px] h-[238px]
+			p-4 pb-24 rounded-lg
+			bg-black-bg
+			border border-black-secondary box-border
 			flex flex-col gap-2
 		">
+			<div
+				className="
+					absolute
+					z-0
+					top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2
+					w-[110px]
+					aspect-square
+					bg-purple
+					blur-[115px]
+					rounded-full
+				"
+			/>
 			{render && <SparkLineChart data={[{ data: data }]} />}
 			<div className="h-16 absolute bottom-4">
 				<p className="text-white font-semibold">
 					{title}:
 					<br />
-					<span className="h h5">{value}</span>
+					<span className="h h5">{prefix}{value}</span>
 				</p>
 			</div >
 		</div >
