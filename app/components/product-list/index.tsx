9397907e980b6ca4c425e37d1e450235022013e5
Pedro Fernandes <thephfernandes@gmail.com>
2023-05-29 17:28:22 -0300

    creates product list component and product/:id page

---
+++ app/components/product-list/index.tsx
@@ -0,0 +1,35 @@
+import { Link } from "@remix-run/react";
+
+export default function Products({ products }) {
+	return (
+		<div className="flex flex-col flex-grow min-w-[50%] text-gray">
+			<div
+				className="
+			w-full h-[55px]
+			text-lg
+			mb-4 last:mb-0
+			flex items-stretch justify-between 
+			border-b border-solid border-black-border
+			overflow-clip
+			">
+				<div className="pl-2 content-box w-[115px] flex items-center">
+					Posição
+				</div>
+				<div className="pl-2 text-start flex-grow flex items-center">
+					Nome do produto
+				</div>
+			</div>
+
+			{products.map((product, index) => {
+				const productId = product.id.split("/").slice(-1);
+
+				return (<div key={index} className="w-full flex h-[55px] items-stretch justify-between rounded-lg overflow-clip mb-4 last:mb-0">
+					<div className="bg-black-secondary py-3 px-6 content-box w-[115px] flex items-center justify-center">{index + 1}</div>
+					<div className="bg-black-border py-3 pl-6 text-start flex-grow flex items-center">{product.title}</div>
+					<Link to={`/products/${productId}`}
+						className="bg-purple py-3 px-12 text-center flex items-center font-semibold text-white">Ver detalhes</Link>
+				</div>)
+			})}
+		</div>
+	);
+};
---
+++ app/routes/_app.products.$productId.tsx
@@ -0,0 +1,68 @@
+import type { LoaderArgs } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { redirect } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
+import { fetchShopifyProductById } from "~/api";
+import { checkAuth } from "~/api/helpers";
+import { toLocalCurrency } from "~/utils/numbers";
+
+export const loader = async ({ request, params }: LoaderArgs) => {
+	const user = await checkAuth(request);
+	if (!user) {
+		return redirect("/login");
+	}
+	const product = await fetchShopifyProductById(request, user, params.productId);
+	return json(product);
+}
+
+export default function SelectedProduct() {
+	const product = useLoaderData();
+
+	const pricesAreDifferent = product.priceRange.minVariantPrice.amount !== product.priceRange.maxVariantPrice.amount;
+
+	return (
+		<div
+			className="
+			relative
+			p-4
+			text-lg
+			flex flex-col gap-4
+			flex-grow max-w-md
+			border border-solid border-black-border rounded-lg
+		">
+			<Link to="/products" className="absolute top-4 right-4 w-[42px] aspect-square bg-black-bg border border-solid border-black-secondary rounded-[4px] flex justify-center items-center hover:border-purple">
+				<img src="/x.svg" alt="sair" />
+			</Link>
+			<h2 className="h5 self-start">{product.title}</h2>
+			<div className="bg-white rounded-lg overflow-clip">
+				<img src={product.featuredImage.url} alt={product.featuredImage.altText} />
+			</div>
+
+			<p className="font-semibold">{product.description}</p>
+			{pricesAreDifferent ?
+				(
+					<>
+						<p className="text-gray"><span className="font-semibold text-white">Preço mínimo: </span>{toLocalCurrency(product.priceRange.minVariantPrice.amount)}</p>
+						<p className="text-gray"><span className="font-semibold text-white">Preço máximo: </span>{toLocalCurrency(product.priceRange.maxVariantPrice.amount)}</p>
+					</>
+				) : (
+					<p className="text-gray"><span className="font-semibold text-white">Preço: </span>{toLocalCurrency(product.priceRange.minVariantPrice.amount)}</p>
+				)
+			}
+			<a
+				target="_blank"
+				rel="noreferrer"
+				href={product.onlineStoreUrl}
+				className="
+				w-full h-[45px]
+				border-box 
+				flex items-center justify-center gap-2
+				bg-transparent
+				border hover:border-2 border-solid border-purple--dark rounded-lg 
+				text-purple--dark text-center p-3
+			">
+				<img src="/icons/exit-top-right.svg" alt="exit icon" />
+				Ver produto</a>
+		</div>
+	)
+}
