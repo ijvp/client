a3d337e227e7a98a41bd92235a7f9e6092e245a4
Pedro Fernandes <thephfernandes@gmail.com>
2023-07-20 18:36:56 -0300

    css

--- app/routes/_app.criativos.tsx
+++ app/routes/_app.criativos.tsx
@@ -1,11 +1,14 @@
 import type { LoaderArgs } from "@remix-run/node";
 import { json, redirect } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
-import { useEffect, useState } from "react";
+import { Combobox, HorizontalStack, Icon, LegacyStack, Listbox, Stack, Tag, TextContainer, TextField, VerticalStack } from "@shopify/polaris";
+import { SearchMinor } from '@shopify/polaris-icons';
+import { useEffect, useState, useMemo, useCallback } from "react";
 import { fetchFacebookAdsInsights } from "~/api/facebook";
 import { checkAuth } from "~/api/helpers";
 import { fetchUserStores } from "~/api/user";
 import Input from "~/components/input";
+import IntervalSelect from "~/components/interval-select";
 import PageTitle from "~/components/page-title";
 
 export const loader = async ({ request }: LoaderArgs) => {
@@ -37,9 +40,23 @@ export function ErrorBoundary() {
 	);
 };
 
+const Headers = {
+	name: "Nome",
+	spend: "Valor investido",
+	impressions: "Impressões",
+	outboundClicks: "Cliques de saída",
+	pageViews: "Visualizações de página",
+	purchases: "Compras",
+	purchasesConversionValue: "Valor de conversão",
+	CTR: "CTR",
+	CPS: "CPS",
+	CPA: "CPA",
+	ROAS: "ROAS"
+};
+
 export default function CreativesPage() {
 	const ads = useLoaderData();
-	const tableHeaders = Object.keys(ads[0]);
+	const tableHeaders = Object.keys(Headers);
 	const [searchTerm, setSearchTerm] = useState("");
 	const [filteredAds, setFilteredAds] = useState(ads);
 
@@ -50,6 +67,79 @@ export default function CreativesPage() {
 	const totalPurchasesConversions = filteredAds.reduce((acc, item) => acc + item.purchasesConversionValue, 0).toFixed(2);
 	const totalPurchases = filteredAds.reduce((acc, item) => acc + item.purchases, 0);
 
+	const columnOptions = useMemo(
+		() => Array.from(Object.entries(Headers).map(entry => {
+			return {
+				value: entry[0],
+				label: entry[1]
+			}
+		})),
+		[]
+	);
+
+
+	const [selectedColumns, setSelectedColumns] = useState(Array.from(Object.keys(Headers)));
+	const [inputValue, setInputValue] = useState("");
+	const [options, setOptions] = useState(columnOptions);
+	const updateText = useCallback(
+		(value: string) => {
+			setInputValue(value)
+			if (value === "") {
+				setOptions(columnOptions);
+				return;
+			}
+
+			const filterRegex = new RegExp(value, 'i');
+			const resultOptions = columnOptions.filter(option => option.label.match(filterRegex));
+			setOptions(resultOptions);
+		},
+		[columnOptions]
+	);
+
+	const updateSelection = useCallback(
+		(selected: string) => {
+			if (selectedColumns.includes(selected)) {
+				setSelectedColumns(selectedColumns.filter(column => column !== selected));
+			} else {
+				setSelectedColumns([...selectedColumns, selected]);
+			}
+
+			updateText("");
+		},
+		[selectedColumns, updateText]
+	);
+
+	const removeTag = useCallback(
+		(tag: string) => () => {
+			const columns = [...selectedColumns];
+			columns.splice(columns.indexOf(tag), 1);
+			setSelectedColumns(columns);
+		},
+		[selectedColumns]
+	);
+
+	const tagsMarkup = selectedColumns.map((column, index) => {
+		return (<Tag key={index} onRemove={removeTag(column)}>
+			{Headers[column]}
+		</Tag>)
+	});
+
+	const columnsMarkup =
+		options.length > 0
+			? options.map((option, index) => {
+				const { label, value } = option;
+				return (
+					<Listbox.Option
+						key={index}
+						value={value}
+						selected={selectedColumns.includes(value)}
+						accessibilityLabel={label}
+					>
+						{label}
+					</Listbox.Option>
+				)
+			}) : null;
+
 	useEffect(() => {
 		const filteredData = ads.filter(item => item.name.toLowerCase().includes(searchTerm.toLowerCase()));
 		setFilteredAds(filteredData);
@@ -59,8 +149,9 @@ export default function CreativesPage() {
 		<>
 			<PageTitle>Criativos</PageTitle>
 			<p className="subtitle">
-				Aqui você tem uma visualização geral de todos os seus criativos ativos pertencentes a campanhas ativas do Facebook Ads. Você pode buscar e agrupar por nome de anúncios para ter uma visão melhor da performance de criativos duplicados!
+				Aqui você tem uma visualização geral de todos os seus criativos pertencentes a campanhas do Facebook Ads. Você pode buscar e agrupar por nome para ter uma visão melhor da performance de criativos duplicados!
 			</p>
+			<IntervalSelect />
 			<Input
 				name="ad-name"
 				type="text"
@@ -69,38 +160,72 @@ export default function CreativesPage() {
 				value={searchTerm}
 				onChange={(e) => setSearchTerm(e.target.value)}
 			/>
+			<div className="flex flex-col gap-4 mb-8">
+				<Combobox
+					allowMultiple
+					activator={
+						<Combobox.TextField
+							prefix={<Icon source={SearchMinor} />}
+							onChange={updateText}
+							label="Selecionar colunas"
+							labelHidden
+							value={inputValue}
+							placeholder="Selecionar colunas"
+							autoComplete="off"
+						/>
+					}>
+					{columnsMarkup ? (
+						<Listbox onSelect={updateSelection}>{columnsMarkup}</Listbox>
+					) : null}
+				</Combobox>
+				<HorizontalStack gap="4">
+					<LegacyStack>{tagsMarkup}</LegacyStack>
+				</HorizontalStack>
+			</div>
 			<table className="w-full mb-12">
 				<thead>
 					<tr>
-						{tableHeaders.map((header: string) => header != 'id' && (<th key={header}>{header}</th>))}
+						{tableHeaders.map((header: string) =>
+							selectedColumns.includes(header) && (<th key={header}>{Headers[header]}</th>))}
 					</tr>
 				</thead>
 				<tbody>
 					{filteredAds.map((ad: any) => (
 						<tr key={ad.id}>
 							{tableHeaders.map(header => header != 'id' && (
-								<td key={header} className="p-2 border border-purple" >
-									{typeof ad[header] === 'number' ?
-										['impressions', 'outboundClicks', 'pageViews', 'purchases'].includes(header) ?
-											ad[header] :
-											ad[header].toFixed(2)
-										: ad[header]}
-								</td>
+								selectedColumns.includes(header) && (
+									<td key={header} className="p-2 border border-purple" >
+										{typeof ad[header] === 'number' ?
+											['impressions', 'outboundClicks', 'pageViews', 'purchases'].includes(header) ?
+												ad[header] :
+												ad[header].toFixed(2)
+											: ad[header]}
+									</td>
+								)
 							))}
 						</tr>
 					))}
 					<tr>
-						<td className="p-2 border border-purple"><b>Total</b></td>
-						<td className="p-2 border border-purple"><b>{totalSpend.toFixed(2)}</b></td>
-						<td className="p-2 border border-purple"><b>{totalImpressions}</b></td>
-						<td className="p-2 border border-purple"><b>{totalClicks}</b></td>
-						<td className="p-2 border border-purple"><b>{totalPageViews}</b></td>
-						<td className="p-2 border border-purple"><b>{totalPurchases}</b></td>
-						<td className="p-2 border border-purple"><b>{totalPurchasesConversions}</b></td>
-						<td className="p-2 border border-purple"><b>{(totalClicks / totalImpressions * 100).toFixed(2)}</b></td>
-						<td className="p-2 border border-purple"><b>{totalPageViews != 0 ? (totalSpend / totalPageViews).toFixed(2) : 0}</b></td>
-						<td className="p-2 border border-purple"><b>{totalPurchases != 0 ? (totalSpend / totalPurchases).toFixed(2) : 0}</b></td>
-						<td className="p-2 border border-purple"><b>{(totalPurchasesConversions / totalSpend).toFixed(2)}</b></td>
+						{selectedColumns.slice(0, 1).map(header => (
+							<td key={header} colSpan={1} className="p-2 border border-purple"><b>Total</b></td>
+						))}
+						{selectedColumns.slice(1).map(header => (
+							<td key={header} className="p-2 border border-purple">
+								<b>
+									{header === 'spend' ? totalSpend.toFixed(2) :
+										header === 'impressions' ? totalImpressions :
+											header === 'outboundClicks' ? totalClicks :
+												header === 'pageViews' ? totalPageViews :
+													header === 'purchases' ? totalPurchases :
+														header === 'purchasesConversionValue' ? totalPurchasesConversions :
+															header === 'CTR' ? (totalClicks / totalImpressions * 100).toFixed(2) :
+																header === 'CPS' ? (totalPageViews != 0 ? (totalSpend / totalPageViews) : 0).toFixed(2) :
+																	header === 'CPA' ? (totalPurchases != 0 ? (totalSpend / totalPurchases) : 0).toFixed(2) :
+																		header === 'ROAS' ? (totalPurchasesConversions / totalSpend).toFixed(2) : ''
+									}
+								</b>
+							</td>
+						))}
 					</tr>
 				</tbody>
 			</table>
