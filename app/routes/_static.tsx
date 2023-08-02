abead464f00d78592d8588962e998741cce5abb7
Pedro Fernandes <thephfernandes@gmail.com>
2023-08-02 11:28:20 -0300

    finish ToS and PP page

--- app/routes/_static.tsx
+++ app/routes/_static.tsx
@@ -5,7 +5,9 @@ export default function Static() {
 		<div className="
 		relative
 		flex flex-col flex-grow
-		m-auto
+		mx-5
+		my-auto
+		md:m-auto
 		max-h-[85vh]
 		border 
 		border-black-secondary
@@ -20,11 +22,25 @@ export default function Static() {
 		after:bg-gradient-to-t
 		after:from-black
 		after:to-transparent
-		after:h-[25%]
+		after:h-[20%]
 		after:z-10
 		">
+			<div
+				className="
+				absolute
+				z-0
+				top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2
+				w-1/6
+				aspect-square
+				bg-purple
+				blur-[115px]
+				rounded-full
+			"
+			/>
 			<div className="overflow-scroll h-full">
-				<Outlet />
+				<div className="p-6 mb-32 md:p-10 lg:p-16 xl:p-20 w-full text-lg relative">
+					<Outlet />
+				</div>
 			</div>
 		</div>
 	)
